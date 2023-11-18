e6ed9de5236dea69477e2202ed3794373f300a7e
Anton <bnderos@mail.ru>
2023-11-18 22:16:35 +0700

    Update query 05.sql

--- 15. SQL/query 05.sql
+++ 15. SQL/query 05.sql
@@ -1,4 +1,5 @@
-
+# 5. Выведите на экран всю информацию о людях, у которых названия аккаунтов в поле network_username содержат подстроку 'money', а фамилия начинается на 'K'.    
+        
 SELECT *
 FROM people
 WHERE twitter_username LIKE '%money%'
