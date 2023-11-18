8f74905b92bec35fdd6331833ba51e1e441c72ad
Anton <bnderos@mail.ru>
2023-11-18 22:13:11 +0700

    Update query 02.sql

--- 15. SQL/query 02.sql
+++ 15. SQL/query 02.sql
@@ -1,4 +1,5 @@
-
+# 2. Отобразите количество привлечённых средств для новостных компаний США. Используйте данные из таблицы company. Отсортируйте таблицу по убыванию значений в поле funding_total .
+        
 SELECT funding_total
 FROM company
 WHERE category_code = 'news'
